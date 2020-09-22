0c30e2bc3dbf6594268804bebf12c74f7466b5bc
jonathan-b-dev <jonathan.billard@halias.fr>
2020-09-22 19:45:00 +0200

    update Home page, add carousel and presentation section

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,8 +1,19 @@
-// CAROUSEL
 $(document).ready(function(){
+    //CAROUSEL
     $('.carousel.carousel-slider').carousel({
-        fullWidth: true
+        fullWidth: true,
+        indicators: true,
+        duration: 200
     });
+
+    autoplay();
+
+    function autoplay() {
+        $('.carousel').carousel('next');
+        setTimeout(autoplay, 4500);
+    }
+    //Mobile Colapse Nav
+    $('.sidenav').sidenav();
 });
   
  
